4df8fd350528b3b6e61f86c15d3721ca5a3c9142
Taras Shypka <tarashypka@gmail.com>
2018-09-11 21:48:24 +0300

    Updated tests for text module

--- tests/vectorizer/test_text.py
+++ tests/vectorizer/test_text.py
@@ -5,31 +5,62 @@ import torch
 from torchlearn.vectorizer import TextTokenizer, EmbeddingTextVectorizer
 
 
-class TextVectorizerTest(unittest.TestCase):
+class TextTokenizerTest(unittest.TestCase):
+
+    def test_tokenize_pattern(self):
+        """Test token_pattern tokenizer parameter"""
+        tokenizer = TextTokenizer(token_pattern='^[^\W\d][^\W\d]+$', max_token_len=8)
+        text = 'a bc def a1 bc2 def3'
+        tokens = tokenizer.tokenize(text)
+        for token in ['a', 'a1', 'bc2', 'def3']:
+            self.assertNotIn(member=token, container=tokens, msg="Invalid token was not removed!")
+        for token in ['bc', 'def']:
+            self.assertIn(member=token, container=tokens, msg="Valid token was removed!")
+
+    def test_tokenize_token_len(self):
+        """Test mak_token_len tokenizer parameter"""
+        tokenizer = TextTokenizer(token_pattern='^[^\W\d][^\W\d]+$', max_token_len=4)
+        text = 'abc abcd abcde'
+        tokens = tokenizer.tokenize(text)
+        self.assertIn(member='abc', container=tokens, msg="Short enough token was removed!")
+        self.assertIn(member='abcd', container=tokens, msg="Short enough token was removed!")
+        self.assertNotIn(member='abcde', container=tokens, msg="Too long token was not removed!")
+
+    def test_tokenize_stopwords(self):
+        """Test stopwords tokenizer parameter"""
+        tokenizer = TextTokenizer(token_pattern='^[^\W\d][^\W\d]+$', max_token_len=8, stopterms=['abc'])
+        text = 'abc def gjk'
+        tokens = tokenizer.tokenize(text)
+        self.assertNotIn(member='abc', container=tokens, msg="Stopword was not removed!")
+        self.assertIn(member='def', container=tokens, msg="Not a stopword was removed!")
+        self.assertIn(member='gjk', container=tokens, msg="Not a stopword was removed!")
+
+
+class EmbeddingTextVectorizerTest(unittest.TestCase):
 
     def test_transform(self):
         """Test that transform() method returns proper vectors"""
-        types = ['a', 'b', 'c', 'd']
+        types = ['abc', 'def', 'gjk', 'lmn']
         embeddings = np.random.random(size=(len(types), 200))
-        t = TextTokenizer()
-        v = EmbeddingTextVectorizer(types=types, embeddings=embeddings, tokenizer=t, seq_len=8)
-        texts = ['a b d', 'unknown d']
-        vectors = v.transform(texts=texts)
+        tokenizer = TextTokenizer()
+        vectorizer = EmbeddingTextVectorizer(types=types, embeddings=embeddings, tokenizer=tokenizer, seq_len=8)
+        texts = ['abc def gjk', 'unknown gjk']
+        vectors = vectorizer.transform(texts=texts)
 
         self.assertEqual(
-            vectors.shape[0], v.seq_len, "First dimension should be sequence length!")
+            vectors.shape[0], vectorizer.seq_len, "First dimension should be sequence length!")
         self.assertEqual(
             vectors.shape[1], len(texts), "Second dimension should be amount of input texts!")
         self.assertEqual(
             vectors.shape[2], embeddings.shape[1], "Third dimension should be embedding dimension!")
 
-        token_to_emb = dict(zip(v.types_, v.embeddings_))
-        pad_true = token_to_emb[v.__PADDING__]
-        unk_true = token_to_emb[v.__UNKNOWN__]
+        token_to_emb = dict(zip(vectorizer.types_, vectorizer.embeddings_))
+        pad_true = token_to_emb[vectorizer.__PADDING__]
+        unk_true = token_to_emb[vectorizer.__UNKNOWN__]
         for text_ind, text in enumerate(texts):
-            tokens = v.tokenize_(text)
+            tokens = tokenizer.tokenize(text)
             text_len = len(tokens)
-            n_paddings = v.seq_len - text_len
+            n_paddings = vectorizer.seq_len - text_len
             for pad_ind in range(n_paddings):
                 pad_got = vectors[pad_ind, text_ind, :]
                 self.assertTrue(torch.all(torch.eq(pad_got, pad_true)), "Wrong padding vector!")
