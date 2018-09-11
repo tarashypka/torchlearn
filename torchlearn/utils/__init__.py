6a69473541c26bf92c4d3e5e32120660c6cf40d5
Taras Shypka <tarashypka@gmail.com>
2018-09-11 19:32:37 +0300

    Updated utils

--- torchlearn/utils/__init__.py
+++ torchlearn/utils/__init__.py
@@ -1,8 +1,9 @@
-from .io import report, dump_pickle, load_pickle, write_lines, read_lines
+from .io import plain_path, report, dump_pickle, load_pickle, write_lines, read_lines
 from .utils import batches
 
 
 __all__ = [
+    "plain_path",
     "report",
     "dump_pickle",
     "load_pickle",
