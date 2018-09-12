a312b432153d7f77fdfba26ba891163e1aa651e6
Taras Shypka <tarashypka@gmail.com>
2018-09-12 23:39:41 +0300

    Added callbacks

---
+++ torchlearn/callback.py
@@ -0,0 +1,38 @@
+from datetime import datetime as dt
+from pathlib import Path
+from time import time
+from typing import *
+
+import torch
+from torch import nn
+
+from torchlearn.utils import report, dump_pickle, Savable
+
+
+class Callback:
+    pass
+
+
+class SaveCallback(Callback):
+
+    def __init__(self, obj_to_save: Any, save_per_min: int, save_name: str):
+        self.obj_to_save = obj_to_save
+        self.save_per_min = save_per_min
+        self.save_name = save_name
+
+        self.prev_save_: int = None
+        self.cache_dir_ = Path('.torchtext')
+        self.cache_dir_.mkdir(exist_ok=True, parents=True)
+
+    def __call__(self, *args, **kwargs):
+        now = time()
+        if self.prev_save_ is None or now - self.prev_save_ > self.save_per_min * 60:
+            save_path = self.cache_dir_ / f'{self.save_name}_{dt.now().strftime("%Y%m%d%H%M%S")}.bin'
+            report('Save', self.save_name, 'into', save_path, '...')
+            if isinstance(self.obj_to_save, Savable):
+                self.obj_to_save.__save__(filepath=save_path)
+            elif isinstance(self.obj_to_save, nn.Module):
+                torch.save(obj=self.obj_to_save, f=save_path)
+            else:
+                dump_pickle(filepath=save_path, obj=self.obj_to_save)
+            self.prev_save_ = now
