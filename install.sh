7f4ac66eb46230a7b228f303e06162a97ca27287
Taras Shypka <tas@jooble.com>
2019-04-03 19:53:12 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-ENV_NAME=$1
+PROJ_DIR=$(dirname $0)
+cd $PROJ_DIR
 
-source activate $ENV_NAME
+ANACONDA=/usr/local/anaconda
+export PATH=$ANACONDA/bin:$PATH
+ENV_NAME=$1
+if [[ $ENV_NAME == "" ]]; then
+    ENV_PATH=$ANACONDA
+else
+    ENV_PATH=$ANACONDA/envs/$ENV_NAME
+    if [[ ! -d $ENV_PATH ]]; then
+        conda create --prefix=$ENV_PATH --file=conda_libs.txt
+    fi
+    source activate $ENV_NAME
+fi
 
 pip install https://download.pytorch.org/whl/cpu/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl
 pip install torchvision
 pip install -r requirements.txt
+pip install .
