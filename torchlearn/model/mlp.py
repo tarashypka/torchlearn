0c292c29c77747c6d956e49ba606ad2708dfacc2
Taras Shypka <tarashypka@gmail.com>
2019-04-04 16:55:39 +0300

    Implemented MLP model

--- torchlearn/model/mlp.py
+++ torchlearn/model/mlp.py
@@ -2,7 +2,9 @@ import os
 from typing import *
 
 import torch
+import numpy as np
 from torch import nn
+from torch.autograd import Variable
 
 from torchlearn.utils import default_device
 
@@ -10,10 +12,27 @@ from torchlearn.utils import default_device
 class MLP(nn.Module):
     """Multi-layer perceptron model"""
 
-    def __init__(self, input_dim: int, hidden_dims: List[int]=None, output_dim: int=2):
+    def __init__(self, input_dim: int, hidden_dims: List[int]=None, output_dim: int=1):
+        super(MLP, self).__init__()
         self.input_dim = input_dim
         self.hidden_dims = hidden_dims
         self.output_dim = output_dim
+
+        self.layers = nn.ModuleList()
+        for input_dim, output_dim in [input_dim] + hidden_dims + [output_dim]:
+            self.layers.append(nn.Linear(in_features=1 + input_dim, out_features=output_dim))
+        self.sigmoid = nn.Sigmoid()
+
+        if self.device == 'cuda':
+            self.input_layer = self.input_layer.cuda()
+            self.hidden_layers = self.hidden_layers.cuda()
+
+    def forward(self, x: torch.Tensor) -> np.array:
+        """Estimate probability of x"""
+        y: Variable = None
+        for layer in self.layers:
+            y = self.sigmoid(layer(x))
+        return y.data.numpy()
 
 
 class LogisticRegression(nn.Module):
@@ -30,9 +49,9 @@ class LogisticRegression(nn.Module):
         if self.device == 'cuda':
             self.weights = self.weights.cuda()
 
-    def forward(self, x: torch.Tensor) -> float:
+    def forward(self, x: torch.Tensor) -> np.array:
         """Estimate probability of x"""
-        return self.sigmoid(self.weights(x))
+        return self.sigmoid(self.weights(x)).data.numpy()
 
     def save(self, filepath: os.PathLike):
         """Save model into binary format"""
