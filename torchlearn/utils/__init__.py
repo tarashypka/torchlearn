b3fd61d9cc575715e8de858de884cd7baef16ac8
Taras Shypka <tarashypka@gmail.com>
2019-04-06 15:44:00 +0300

    utils: Implemented estimate_hidden_dim()

--- torchlearn/utils/__init__.py
+++ torchlearn/utils/__init__.py
@@ -1,2 +1,2 @@
 from .io import Savable, Loadable, report, plain_path, dump_pickle, load_pickle, write_lines, read_lines, clear_dir
-from .utils import batches, default_device, avg_loss
+from .utils import batches, default_device, avg_loss, estimate_hidden_dim
--- torchlearn/utils/utils.py
+++ torchlearn/utils/utils.py
@@ -19,3 +19,23 @@ def avg_loss(curr_loss: float, prev_loss: float, curr_weight: float=0.1, prev_we
     if prev_loss is None:
         prev_loss = curr_loss
     return prev_weight * prev_loss + curr_weight * curr_loss
+
+
+def estimate_hidden_dim(dataset_size: int, input_dim: int, n_layers: int, penalty: float=0.75) -> int:
+    """Estimate hidden dim based on dataset and input size"""
+    # Rule of thumb (taken from LearningFromData course):
+    #   N ~ 10 * d_vc (must be at least, that's why penalty)
+    #   d_vc ~ (input_dim + 1) * layer_1_dim + (layer_1_dim + 1) * layer_2_dim + ... + (layer_n_dim + 1) * 1
+    #   Restriction: layer_1_dim = layer_2_dim = ... = layer_n_dim = hidden_dim
+    if n_layers == 1:
+        hidden_dim = dataset_size / 10 / (input_dim + 1)
+    elif n_layers > 1:
+        a = 1
+        b = (n_layers + input_dim + 1) / (n_layers - 1)
+        c = - dataset_size / 10 / (n_layers - 1)
+        d = (b ** 2 - 4 * a * c)
+        hidden_dim = (- b + d ** 0.5) / 2 / a
+    else:
+        raise ValueError(f'Invalid value of n_layers = {n_layers}, must be positive!')
+    hidden_dim = hidden_dim * penalty
+    return int(hidden_dim)
