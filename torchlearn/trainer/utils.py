9db5a3c9f83b73cb86bf47284595ac384b9149b0
Taras Shypka <tarashypka@gmail.com>
2019-06-21 00:13:05 +0300

    Removed redundant modules

--- torchlearn/trainer/utils.py
+++
@@ -1,7 +0,0 @@
-from torch import optim
-
-
-def adjust_optimizer(optimizer: optim.Optimizer, learning_rate: float) -> optim.Optimizer:
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = learning_rate
-    return optimizer
