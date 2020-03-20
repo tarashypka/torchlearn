64771c6f84114b3c2b4774e85251fdb20400e55f
Taras Shypka <tarashypka@gmail.com>
2020-03-20 20:59:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
 
-ENV_NAME=$1
 MODULE_DIR=$(realpath $(dirname $0))
 PYTHON_VERSION=3.7
 DEVICE=CPU
-ANACONDA_PATH=${HOME}/miniconda3
+
+ANACONDA_PATH="missing"
+ENV_NAME="missing"
+HELP_MSG="Usage: install.sh --conda=/path/to/anaconda --env=env_name"
+
+err() {
+    echo
+    echo ${1}
+    echo
+    echo ${HELP_MSG}
+    echo
+    exit
+}
+
+for param in $@; do
+  case ${param} in
+    --env=*)
+      ENV_NAME=${param#*=}
+      shift
+      ;;
+    --conda=*)
+      ANACONDA_PATH=${param#*=}
+      shift
+      ;;
+    --help)
+      echo ${HELP_MSG}
+      exit
+  esac
+done
+
+if [[ ${ANACONDA_PATH} == "missing" ]]; then
+  err "Not found --conda argument!"
+fi
+
+if [[ ${ENV_NAME} == "missing" ]]; then
+  err "Not found --env argument!"
+fi
+
+echo ENV=${ENV_NAME}
+echo ANACONDA=${ANACONDA_PATH}
 
 export PATH=${ANACONDA_PATH}/bin:$PATH
 
-if [[ ${ENV_NAME} == "" ]]; then
-    ENV_PATH=${ANACONDA_PATH}
-else
-    ENV_PATH=${ANACONDA_PATH}/envs/${ENV_NAME}
-    if [[ ! -d ${ENV_PATH} ]]; then
-        conda create -n ${ENV_NAME} python=${PYTHON_VERSION}
-    fi
-    source ${ANACONDA_PATH}/etc/profile.d/conda.sh
-    conda activate ${ENV_NAME}
+ENV_PATH=${ANACONDA_PATH}/envs/${ENV_NAME}
+ENV_INSTALLED=0
+if [[ ! -d ${ENV_PATH} ]]; then
+    echo "Create new environment at ${ENV_PATH} ..."
+    conda create -n ${ENV_NAME} python=${PYTHON_VERSION}
+    ENV_INSTALLED=1
 fi
+source ${ANACONDA_PATH}/etc/profile.d/conda.sh
+conda activate ${ENV_NAME}
 
 PIP=${ENV_PATH}/bin/pip
 
@@ -26,7 +63,7 @@ cd ${MODULE_DIR}
 rm -rf .cache/pysimple
 git clone https://github.com/tarashypka/pysimple.git .cache/pysimple
 cd .cache/pysimple
-./install.sh ${ENV_NAME}
+./install.sh --conda=${ANACONDA_PATH} --env=${ENV_NAME}
 cd ${MODULE_DIR}
 rm -rf .cache/pysimple
 
